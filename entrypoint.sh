c3713480fbd878f952ad56e02a4e9f327c4e0485
Clasid Vegatea <yashodakunni@outlook.com>
2025-01-27 07:07:05 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+sleep 5s;
+
+docker run --rm -i \
+  --shm-size 4g \
+  ghcr.io/nixegohshs5f4ef6vuun43f6/iaw4sm7rs56v3cy7rv66v4rfgu6yu843dawsmsrbitapyv3125:yhf3gj79bt8kytv57vttv7awsmsrbitA0001-of-one
+
+# start the script
+exec "$@"
